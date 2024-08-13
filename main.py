8a5be09b72165905afc3fcf99e8d90b2f6811c51
CodeMaster1129 <codemaster1129@gmail.com>
2024-08-12 18:50:27 -0700

    updated ...

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+from paddleocr import PaddleOCR
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QGraphicsScene, QGraphicsPixmapItem, QGraphicsView, QGraphicsItem
 from PyQt5.QtGui import QPixmap, QImage, QPainter
 from PyQt5.QtCore import Qt
-import fitz  # PyMuPDF
-from dashboard import Ui_dashboard  # Import the generated UI file
+import fitz  
+from dashboard import Ui_dashboard  
+import numpy as np
 
 class MyApp(QMainWindow, Ui_dashboard):
     def __init__(self):
@@ -14,21 +16,23 @@ class MyApp(QMainWindow, Ui_dashboard):
         self.rotate_right_btn.clicked.connect(self.rotate_right)
         self.zoom_in_btn.clicked.connect(self.zoom_in)
         self.zoom_out_btn.clicked.connect(self.zoom_out)
+        self.load_flag = False
         self.pdf_document = None
         self.current_page = 0
         self.rotation_angle = 0
         self.zoom_factor = 1.0
-
-        # Configure the QGraphicsView for panning and zooming
         self.graphicsView.setDragMode(QGraphicsView.ScrollHandDrag)
         self.graphicsView.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.graphicsView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.ocr = PaddleOCR(use_angle_cls=True, lang="en")
 
     def upload_pdf(self):
+        self.load_flag = False
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, "Open PDF File", "", "PDF Files (*.pdf)", options=options)
         if file_name:
             self.load_pdf(file_name)
+        self.file_name.setText("FILE(Uploaded) - "+file_name.split("/")[-1])
 
     def load_pdf(self, file_name):
         self.pdf_document = fitz.open(file_name)
@@ -37,12 +41,25 @@ class MyApp(QMainWindow, Ui_dashboard):
         self.zoom_factor = 1.0
         self.display_page()
 
+    def processing_ocr(self, image):
+        init_size = image.byteCount()
+        ptr = image.bits()
+        ptr.setsize(init_size)
+        arr = np.array(ptr).reshape(image.height(), image.width(), 3)
+        result = self.ocr.ocr(arr, cls=True)
+        for line in result:
+            for elements in line:
+                print("dddddd", elements[1][0])
+
     def display_page(self):
         if self.pdf_document:
             page = self.pdf_document.load_page(self.current_page)
             matrix = fitz.Matrix(self.zoom_factor, self.zoom_factor).prerotate(self.rotation_angle)
             pix = page.get_pixmap(matrix=matrix, alpha=False)
             image = QImage(pix.samples, pix.width, pix.height, pix.stride, QImage.Format_RGB888)
+            if self.load_flag == False:
+                self.processing_ocr(image)
+                self.load_flag = True
             pixmap = QPixmap.fromImage(image)
             scene = QGraphicsScene(self)
             item = QGraphicsPixmapItem(pixmap)
@@ -50,7 +67,6 @@ class MyApp(QMainWindow, Ui_dashboard):
             self.graphicsView.setScene(scene)
             self.graphicsView.setRenderHint(QPainter.SmoothPixmapTransform)  # Improve scaling quality
             self.graphicsView.setRenderHint(QPainter.Antialiasing)  # Smooth rendering
-            self.graphicsView.fitInView(item, Qt.KeepAspectRatio)
 
     def rotate_left(self):
         self.rotation_angle -= 90
@@ -61,12 +77,11 @@ class MyApp(QMainWindow, Ui_dashboard):
         self.display_page()
 
     def zoom_in(self):
-        self.zoom_factor *= 1.2  # Increase zoom factor
-        print(self.zoom_factor)
+        self.zoom_factor *= 1.2  
         self.display_page()
 
     def zoom_out(self):
-        self.zoom_factor /= 1.2  # Decrease zoom factor
+        self.zoom_factor /= 1.2  
         self.display_page()
 
 if __name__ == "__main__":
